88e98d5d309eb4268d755bc675cfe22bfa48b80d
Paweł Kęska <projekty@pawelkeska.eu>
2025-02-04 21:09:42 +0100

    Remove unused service

--- libs/api/core/src/passport/strategies/jwt-refresh.strategy.ts
+++ libs/api/core/src/passport/strategies/jwt-refresh.strategy.ts
@@ -2,10 +2,10 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Inject, Injectable } from '@nestjs/common';
 
-import { AuthUserData, User } from '@owl-app/lib-contracts';
+import { AuthUserData } from '@owl-app/lib-contracts';
 
 import { JWT_CONFIG_PROVIDER, type JwtConfig } from '../../config/jwt';
-import { IJwtTokenPayload, IJwtTokenService } from '../jwt-token.interface';
+import { IJwtTokenPayload } from '../jwt-token.interface';
 import { extractJWT } from '../extract-jwt';
 
 @Injectable()
@@ -13,8 +13,6 @@ export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'jwt-refresh'
   constructor(
     @Inject(JWT_CONFIG_PROVIDER)
     jwtConfig: JwtConfig,
-    @Inject(IJwtTokenService)
-    jwtTokenService: IJwtTokenService<User>
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
